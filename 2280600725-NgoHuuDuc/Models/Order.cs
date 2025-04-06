0e2aafe62eef488f1fe0d253205718e99a83b94b
Phan Ngọc Cường <cuongharryit@gmail.com>
2025-04-06 22:05:22 +0700

    Implement order management functionality with OrderController, Order model, and OrderStatus enum; add views for order details and index; update sidebar navigation.

--- 2280600725-NgoHuuDuc/Models/Order.cs
+++ 2280600725-NgoHuuDuc/Models/Order.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using NgoHuuDuc_2280600725.Models.Enums;
 
 namespace NgoHuuDuc_2280600725.Models
 {
@@ -9,8 +11,16 @@ namespace NgoHuuDuc_2280600725.Models
         public string? UserId { get; set; }
         public DateTime OrderDate { get; set; } = DateTime.Now;
         public decimal TotalPrice { get; set; }
+
+        [Required(ErrorMessage = "Địa chỉ giao hàng không được để trống")]
+        [Display(Name = "Địa chỉ giao hàng")]
         public string ShippingAddress { get; set; } = "";
+
+        [Display(Name = "Ghi chú")]
         public string? Notes { get; set; }
+
+        [Display(Name = "Trạng thái")]
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
 
         [ForeignKey("UserId")]
         [ValidateNever]
